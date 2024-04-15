e0efbe641b3f3a8a702a169d25ad249a39de0932
diegohenao <dadegad@gmail.com>
2024-04-15 09:47:59 -0500

    iniciando con el proyecto esperanza

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,43 @@
+<!doctype html>
+<html>
+<header>
+      <nav>
+       
+            <h1 class="text-3xl font-bold underline">
+              <center>CENTRO PEDAGOJICO LA ESPERANZA</center>
+            </h1>
+           
+        </div>
+  
+        <div>
+            <ul>
+                <li><br><a href="#">PORTAL DOCENTE</a></li>
+                <li>
+                    <br><a href="#">INGRESO</a>
+                    <!-- submenu -->
+                    <ul>
+                        <li><a href="#">ALUMNOS</a></li>
+                        <li><a href="#">DOCENTES</a></li>
+                        <li><a href="#">PAGOS</a></li>
+                    </ul>
+                </li>
+                <li><a href="#">COMUNIDAD EDUCATIVA</a></li>
+   
+              </ul>
+        </div>
+       </nav>
+    </header>
+<head><br>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  @vite('resources/css/app.css')
+</head>
+<body>
+  <h1 class="text-1xl font-bold underline">
+    <center>EDUCACION DE CALIDAD</center>
+  </h1>
+  <br><p align="center">Entendemos que una educación de calidad es aquella que forma mejores seres humanos, ciudadanos con valores éticos, respetuosos de lo público, que ejercen los derechos humanos, cumplen con sus deberes y conviven en paz. Una educación que genera oportunidades legítimas de progreso y prosperidad para ellos y para el país.
+
+</p>
+</body>
+</html>
